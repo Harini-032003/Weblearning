462adf0bbbe16574ab22e7f5cb4724ca86bf0005
Harini-032003 <hariniv200303@gmail.com>
2023-11-07 20:17:09 +0530

    successfully committed files

---
+++ React.js/src/index.js
@@ -0,0 +1,16 @@
+//react code 
+import React from "react";
+import ReactDOM from "react-dom";
+
+const fName = "Angela";
+const lName = "Yu";
+const num = 7;
+
+ReactDOM.render(
+  <div>
+    <h1>Hello {fName + " " + lName}!</h1>
+    <p>Your lucky number is {num}</p>
+  </div>,
+  document.getElementById("root")
+);
+//run this in code sandbox. wont run in vscode. just for reference
